52d11d0f02166407b139cce58d3fedbfa650c2a6
AtikRahman <atik321@gmail.com>
2020-01-24 02:13:52 +0200

    finetune and copy function

--- js/app-backup.js
+++ js/app-backup.js
@@ -1,36 +1,38 @@
-// all diclarations
 const inputRows = document.querySelector('#input_rows')
 const inputColumns = document.querySelector('#input_columns')
 const inputTableWidth = document.querySelector('#input_table_width')
 const inputTdBgColor = document.querySelector('#td-bg-color')
 const inputThBgColor = document.querySelector('#th-bg-color')
 const inputBorderBgColor = document.querySelector('#border-bg-color')
-const inputThBorderPx = document.querySelector('#th-border-px')
 const inputBorderPx = document.querySelector('#border-px')
 const inputStripBgColor = document.querySelector('#strip-bg-color')
 const inputFontColor = document.querySelector('#font-color')
 const inputFontSize = document.querySelector('#font-size')
-const inputFontWeight = document.querySelector('#font-weight')
 const inputTextAlign = document.querySelector('#text-align')
 const inputFontFamily = document.querySelector('#font-family')
+
 const table = document.querySelector('#genarate_table')
 const getCode = document.querySelector('#get_code')
 const btnGenerate = document.querySelector('#btn_genarate')
 const btnGetCode = document.querySelector('#btn_getcode')
-const btnCopy = document.querySelector('.btn_primary--copy')
-const tableWraper = document.querySelector('.table-genarate-wrap')
 const formContent = document.querySelector('.form-content')
-const borderColloups = document.querySelector('#border-colloups')
-const getHtmlCode = document.querySelector('.textarea').value
 
-// copy function
+// Copy function
 const copyFunction = (element) => {
-    let copyText = document.querySelector('#get_code')
-    copyText.select();
+    let textArea = document.createElement("textarea");
+    textArea.value = element.textContent; //hexacode isthe  element which's content to be copied
+    document.body.appendChild(textArea);
+    textArea.select();
     document.execCommand("Copy");
+    textArea.remove()
 }
+btnGetCode.addEventListener('click', function () {
+    let myCode = table.innerHTML
+    getCode.style.display = 'block'
+    getCode.append(myCode)
+})
 // row genarato
-const myTable = () => {
+btnGenerate.addEventListener('click', function () {
     let userInputRows = inputRows.value
     let userInputColumns = inputColumns.value
     let userInputTableWidth = inputTableWidth.value
@@ -38,42 +40,42 @@ const myTable = () => {
     let userInputThBgColor = inputThBgColor.value
     let userInputBorderBgColor = inputBorderBgColor.value
     let userinputBorderPx = inputBorderPx.value
-    let userinputThBorderPx = inputThBorderPx.value
     let userInputStripBgColor = inputStripBgColor.value
     let userInputFontColor = inputFontColor.value
     // let userInputFontStyle = inputFontStyle.value
     let userInputFontSize = inputFontSize.value
-    let userInputFontWeight = inputFontWeight.value
     let userInputTextAlign = inputTextAlign.value
     let userInputFontFamily = inputFontFamily.value
-    let userInputBorderColloups = borderColloups.value
     table.innerHTML = ''
+
     const tableGenerator = () => {
-        const tHeadGenarator = () => {
+        const thHeadGenarator = () => {
             for (let i = 0; i < userInputColumns; i++) {
                 let tHead = document.createElement('th')
-                tHead.textContent = `Heading ${i + 1}`
-                tHead.style.borderBottom = userinputThBorderPx + 'px' + ' solid' + userInputBorderBgColor
-                tHead.style.borderTop = userinputThBorderPx + 'px' + ' solid' + userInputBorderBgColor
-                tHead.style.border = userInputBorderColloups
-                tHead.style.background = userInputThBgColor
+                tHead.textContent = `Head ${i + 1}`
+                tHead.style.border = userinputBorderPx + 'px' + ' solid' + userInputBorderBgColor
+                tHead.style.background = '#ccc' || userInputThBgColor
                 tHead.style.textAlign = userInputTextAlign
                 tHead.style.fontFamily = userInputFontFamily
                 table.appendChild(tHead)
             }
         }
-        tHeadGenarator()
+        thHeadGenarator()
+
         for (let i = 0; i < userInputRows; i++) {
             const row = document.createElement('tr')
+
             if (i % 2 == 0) {
                 row.style.background = userInputStripBgColor
             }
+
             table.appendChild(row)
+
             const columnsGenarator = () => {
                 for (let i = 0; i < userInputColumns; i++) {
                     let columns = document.createElement('td')
-                    columns.textContent = 'Value'
-                    columns.style.borderBottom = userinputBorderPx + 'px' + ' solid' + userInputBorderBgColor
+                    columns.textContent = 'td column'
+                    columns.style.border = userinputBorderPx + 'px' + ' solid' + userInputBorderBgColor
                     columns.style.textAlign = userInputTextAlign
                     columns.style.fontFamily = userInputFontFamily
                     row.appendChild(columns)
@@ -83,29 +85,26 @@ const myTable = () => {
         }
     }
     tableGenerator()
+
     let tdWidth = table.style.width = userInputTableWidth + '%'
     let tdFont = table.style.color = userInputFontColor
     let tdFontSize = table.style.fontSize = userInputFontSize + 'px'
+
     // let tdFontStyle = table.style.font-family = userInputFontColor
     let tdBg = table.style.background = userInputTdBgColor
     // let tdfontFamily = table.style.fontFamily = userInputFontFamily
-    // let tdBorderColloups = table.style.border = userInputBorderColloups
-    let tdFontWeight = table.style.fontWeight = userInputFontWeight
-}
-btnGenerate.addEventListener('click', function () {
-    tableWraper.style.display = 'block'
-    myTable()
-})
-formContent.addEventListener('click', function () {
-    tableWraper.style.display = 'block'
-    myTable()
-})
-btnGetCode.addEventListener('click', function () {
-    let myCode = table.innerHTML
-    getCode.style.display = 'block'
-    btnCopy.style.display = 'block'
-    getCode.append(myCode)
-})
-btnCopy.addEventListener('click', function () {
-    copyFunction()
+
+    const liveFunction = () => {
+        formContent.addEventListener('click', function () {
+            table.innerHTML = ""
+            tableGenerator()
+        })
+    }
+    liveFunction()
+
+    btnGetCode.addEventListener('click', function () {
+        let myCode = table.innerHTML
+        getCode.style.display = 'block'
+        getCode.append(myCode)
+    })
 })
